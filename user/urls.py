67374eb301d051a4c1d2a661e8cc37fbf90bf1f3
Bonish Thapa <bonishthapa@gmail.com>
2021-05-09 16:30:02 +0545

    simplejwt url edit

--- user/urls.py
+++ user/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+# )
 from user import views
 
 urlpatterns = [
     path('',views.home),
 
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 
     path('designation/list', views.DesignationListView.as_view(),name='designation'),
