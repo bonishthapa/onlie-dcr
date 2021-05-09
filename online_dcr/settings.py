152dcfa189d6562843ad86bff9358ef7504dc46d
Bonish Thapa <bonishthapa@gmail.com>
2021-05-09 16:27:27 +0545

    simplejwt view

--- online_dcr/settings.py
+++ online_dcr/settings.py
@@ -44,14 +44,14 @@ MIDDLEWARE = [
 
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': [
-       'rest_framework.permissions.IsAuthenticated',
-    ]
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     ),
+#     'DEFAULT_PERMISSION_CLASSES': [
+#        'rest_framework.permissions.IsAuthenticated',
+#     ]
+# }
 
 ROOT_URLCONF = 'online_dcr.urls'
 
